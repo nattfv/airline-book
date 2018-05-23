2e86621c15f0e56349385e4b9ba411f917d36864
Branskins <andresje17@gmail.com>
2018-05-22 23:14:10 -0600

    finaliza Vuelo

---
+++ source/Vuelo.cpp
@@ -0,0 +1,39 @@
+#include "Vuelo.h"
+
+Vuelo::Vuelo(string _identificacion, Avion * _avion, Destino* _destino, Piloto * _piloto)
+{
+	identificacion = _identificacion;
+	avion = new Avion(*_avion); // constructor copia
+	destino = new Destino(*_destino); // constructor copia
+	piloto = new Piloto(*_piloto); // constructor copia
+}
+
+Vuelo::Vuelo(const Vuelo & _v)
+{
+	identificacion = _v.identificacion;
+	avion = new Avion(*_v.avion);
+	destino = new Destino(*_v.destino);
+	piloto = new Piloto(*_v.piloto);
+}
+
+Vuelo::~Vuelo()
+{
+	delete avion;
+	delete destino;
+	delete piloto;
+}
+
+void Vuelo::prepararAvion()
+{
+	avion->crearAsientos();
+}
+
+string Vuelo::mostrarVuelo()
+{
+	stringstream s;
+	s << "Identificacion: " << identificacion << "\n";
+	s << destino->mostrarDestino()
+		<< avion->mostrarAvion()
+		<< piloto->mostrarPiloto();
+	return s.str();
+}
--- source/Vuelo.h
+++ source/Vuelo.h
@@ -11,4 +11,9 @@ private:
 	Destino* destino; //creado por el constructor copia
 	Piloto* piloto; //creado por el constructor copia
 public:
+	Vuelo(string _identificacion, Avion* _avion, Destino* _destino, Piloto* _piloto);
+	Vuelo(const Vuelo& _v);
+	~Vuelo();
+	void prepararAvion();
+	string mostrarVuelo();
 };

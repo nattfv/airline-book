d32c7acfaf1dcb9ef4b02a570df75c3315515f52
Branskins <andresje17@gmail.com>
2018-05-26 16:04:51 -0600

    metodos para procesar lectura de archivos

--- source/utiles.cpp
+++ source/utiles.cpp
@@ -10,6 +10,8 @@ int obtenerValorEntero(int minimo, int maximo)
 			throw ExcepcionMinimo(minimo, maximo, evaluar);
 		if (evaluar > maximo && maximo != 0) // max != 0; cuando no se sabe el maximo aun
 			throw ExcepcionMinimo(minimo, maximo, evaluar);
+		cin.clear();
+		cin.ignore(255, '\n');
 	}
 	else
 	{
@@ -18,4 +20,36 @@ int obtenerValorEntero(int minimo, int maximo)
 		throw ExcepcionCadena("un numero entero");
 	}
 	return evaluar;
+}
+
+string procesarHilera(ifstream& archivo)
+{
+	string hilera;
+	getline(archivo, hilera, '\n');
+	return hilera;
+}
+
+int procesarInt(stringstream & particion, char delimitador)
+{
+	string cadenaStr;
+	int cadenaInt;
+	getline(particion, cadenaStr, delimitador);
+	cadenaInt = atoi(cadenaStr.c_str());
+	return cadenaInt;
+}
+
+bool procesarBoolean(stringstream & particion, char delimitador)
+{
+	string cadenaStr;
+	getline(particion, cadenaStr, delimitador);
+	return cadenaStr == "1";
+}
+
+double procesarDouble(stringstream & particion, char delimitador)
+{
+	string cadenaStr;
+	double cadenaDouble;
+	getline(particion, cadenaStr, delimitador);
+	cadenaDouble = atof(cadenaStr.c_str());
+	return cadenaDouble;
 }
--- source/utiles.h
+++ source/utiles.h
@@ -3,5 +3,12 @@
 #include"ExcepcionMaximo.h"
 #include"ExcepcionMinimo.h"
 #include<iostream>
+#include<fstream>
 
+//para validaciones
 int obtenerValorEntero(int minimo, int maximo);
+//para archivos
+string procesarHilera(ifstream& archivo);
+int procesarInt(stringstream& particion, char delimitador);
+bool procesarBoolean(stringstream& particion, char delimitador);
+double procesarDouble(stringstream & particion, char delimitador);

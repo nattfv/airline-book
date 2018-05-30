501fb45fc7df80378063308afa7f75547b6ce334
Branskins <andresje17@gmail.com>
2018-05-30 17:16:23 -0600

    base de controlador

---
+++ source/Controlador.h
@@ -0,0 +1,28 @@
+#pragma once
+#include"InterfazDestino.h"
+#include"InterfazPiloto.h"
+#include"InterfazAvion.h"
+#include"InterfazVendedor.h"
+#include"InterfazVuelo.h"
+#include"InterfazReservacion.h"
+#include"ExcepcionExistencia.h"
+
+class Controlador
+{
+private:
+	Aerolinea* aerolinea;
+public:
+	Controlador();
+	~Controlador();
+	void controlPrincipal();
+	bool controlSecundario();
+	bool controlAdministracion();
+	bool controlDestinos();
+	bool controlPilotos();
+	bool controlAviones();
+	bool controlVuelos();
+	bool controlVendedores();
+	bool controlReservacion();
+	void controlFlujoEntrada();
+	void controlFlujoSalida();
+};
